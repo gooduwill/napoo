eaa84803602dead15457b7fc16d58f94e796f2e6
gooduwill <goodyouwill@gmail.com>
2025-10-13 20:50:37 +0530

    red new msg

--- rating.js
+++ rating.js
@@ -19,7 +19,7 @@ for(let i=0;i<numbers.length;i++){
     console.log(numbers[i])
 }
 console.log(numbers); */
-const numbers=[10,11,12,13,14];
+const numbers=[10,11,12,13,15];
 numbers.forEach((ele)=>{
     console.log(ele)
 
